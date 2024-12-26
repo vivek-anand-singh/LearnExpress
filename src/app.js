8362e4c6afcf4a3b22deda256ca4897cbdea3c64
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-12-26 20:09:50 +0530

    Testing updating and deleting apis

--- src/app.js
+++ src/app.js
@@ -15,6 +15,68 @@ app.post("/signup", async (req,res) =>{
     }
 })
 
+
+//get user by email
+app.get("/user", async (req,res)=>{
+    const email = req.body.email;
+
+    try{
+        const users = await User.findOne({email:email})
+        if(!users){
+            return res.status(404).send("User not found");
+        }  
+        res.send(users);
+    } catch(e){
+        res.status(400).send(e.message);
+    }
+
+})
+
+// get all users
+app.get("/feed", async (req,res) =>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch(e){
+        res.status(400).send(e.message);
+    }
+})
+
+// delete the user by ID
+app.delete("/users", async (req,res) =>{
+    try{
+        const user = await User.findByIdAndDelete(req.body.id);
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+        res.send(user);
+    } catch(e){
+        res.status(500).send(e.message);
+    }
+});
+
+
+//update the user by ID
+app.patch("/users", async (req,res) =>{
+    try{
+        const user = await User.findByIdAndUpdate(req.body.id, req.body);
+        res.send("Succesfully updated the user");
+    } catch(e){
+        res.status(400).send(e.message);
+    }
+}
+) 
+
+app.patch("/usersemail", async (req,res) =>{
+    try{
+        const user = await User.findOneAndUpdate({email:req.body.email}, req.body);
+        res.send("Succesfully updated the user");
+    } catch(e){
+        res.status(400).send(e.message);
+    }
+});
+
+
 connectDB().then(() => {
     console.log('Connected to MongoDB');
     app.listen(3000, () => {
