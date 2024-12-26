bc51071c0ce178aa09da164edf0be005e12b8893
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-12-26 18:04:55 +0530

    Saving the user succesfully

--- src/app.js
+++ src/app.js
@@ -3,21 +3,15 @@ const connectDB =  require('./config/database');
 const app  = express();
 const User = require('./models/user');
 
-app.post("/signup", async (req,res) =>{
-    const userObj = {
-        firstName: "Akshay",
-        lastName: "Saini",
-        email: "akshay@gmail.com",
-        password: "12",
-    }
-    // Creating a new instance of the user model
-    const user = new User(userObj);
+app.use(express.json());
 
+app.post("/signup", async (req,res) =>{
+    const user = new User(req.body);
     try{
-        await user.save(); // Saving the user to the database
-        res.send("User Created");
-    } catch(err){
-        res.status(400).send("Error while creating the user", err.message); 
+        await user.save();
+        res.send("User saved successfully");
+    }catch(e){
+        res.status(400).send("Error saving the user"+e.message);
     }
 })
 
