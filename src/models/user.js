88207b47f580fabe4f7b7ae923cf38de44cf4890
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-12-27 12:04:52 +0530

    Updated the schema and saw how does the shema properties of schema options

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,48 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
     },
     lastName:{
         type:String,
     },
     email:{
         type:String,
+        required:true,
+        lowecase:true,
+        unique:true,    
+        trim:true,
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
         type: Number,
+        min:18,
     },
     gender:{
         type: String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                return new Error("Gender is invalid"); 
+            }
+        }
+    },
+    photourl:{
+        type: String,
+        default: ".com"
+    },
+    about:{
+        type: String,
+        default: "Hey there! I am using Social Media App",
+    },
+    skills:{
+        type: [String],
     }
-});
+},
+{
+    timestamps:true,
+});  
 
 module.exports = mongoose.model('User', userSchema);
