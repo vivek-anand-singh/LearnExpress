03b7574fc3a2b999ca05dfd4e54dcd6b5f723359
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-12-29 12:53:59 +0530

    Applied database and indexing

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -22,6 +22,8 @@ const connectionRequestSchema = new mongoose.Schema({
     timestamps: true
 });
 
+connectionRequestSchema.index({fromUserId: 1, toUserId: 1}, {unique: true});
+
 connectionRequestSchema.pre("save", function(next){
     const connectionRequest = this;
     if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
